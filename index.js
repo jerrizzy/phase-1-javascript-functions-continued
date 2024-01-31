79bf752b5b8bf250c0ee28b997db19369c1def6a
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-01-31 09:49:35 -0500

    anon function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+ function saturdayFun(activity = "roller-skate") {
+     return `This Saturday, I want to ${activity}!`
+ }
+
+ let mondayWork = function(act = "go to the office") {
+     return `This Monday, I will ${act}.` 
+ }
+
+ let wrapAdjective = function(style = "*") {
+     return function(adj="a hard worker") {
+         return `You are ${style}${adj}${style}!`
+     }
+ }
